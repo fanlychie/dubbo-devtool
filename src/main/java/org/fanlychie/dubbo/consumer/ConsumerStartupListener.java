dd7d25f80f981ab4889f6bbbdc2bdbaec7689ca0
fanlychie <fanlychie@yeah.net>
2018-01-08 17:05:59 +0800

    备注

--- src/main/java/org/fanlychie/dubbo/consumer/ConsumerStartupListener.java
+++ src/main/java/org/fanlychie/dubbo/consumer/ConsumerStartupListener.java
@@ -1,7 +1,6 @@
 package org.fanlychie.dubbo.consumer;
 
 import org.fanlychie.dubbo.DubboConfigHandler;
-import org.fanlychie.dubbo.util.ConsoleLogger;
 import org.fanlychie.dubbo.util.RuntimeArgument;
 
 import javax.servlet.ServletContextEvent;
@@ -17,10 +16,8 @@ public class ConsumerStartupListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         // 通过启动参数：[ -Ddebug | -Ddubbo.local.debug]
         if (RuntimeArgument.isDirectConnectProvider()) {
-            // 启用直连提供者
+            // DUBBO消费者服务启动前的预处理工作
             DubboConfigHandler.preHandleConsumer(servletContextEvent.getServletContext().getInitParameter("contextConfigLocation"));
-            // 启动日志
-            ConsoleLogger.logStartupConsumer();
         }
     }
 
